0dc399d19c987e2834462f0cee5a8ecbb1ca5b0c
emanueleplantone <plantone.emanuele@iisgalvanimi.edu.it>
2022-03-10 09:01:35 +0000

    finito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Output,Input,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Empty-Project';
+  testo:string=""
+  testo_casella:string=""
+
+  onClick(){
+    console.log("Ciao Mondo")
+    this.testo=this.testo_casella
+  }
+
+
+
 }
